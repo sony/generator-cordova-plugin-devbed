9b166e4094becb0d35426f19c72b35ccd55e9e49
yseki <Yasuharu.Seki@jp.sony.com>
2014-12-15 17:47:16 +0900

    Modified to create the cordova plugin using with plugman'

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 'use strict';
+var assert = require('assert');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var plugman = require('plugman');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -17,46 +19,86 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'pluginName',
+      message: 'Enter the name of the plugin.',
+      default: 'CoolPlugin'
+    },
+    {
+      type: 'input',
+      name: 'pluginID',
+      message: 'Enter an ID for the plugin. ex: org.bar.foo',
+      default: 'org.cool.plugin'
+    },
+    {
+      type: 'input',
+      name: 'pluginVersion',
+      message: 'Enter a version for the plugin. ex: 0.0.1',
+      default: '0.0.1'
+    },
+    {
+      type: 'checkbox',
+      name: 'pluginPlatforms',
+      message: 'Enter the platforms the plugin supports. ex: android, ios',
+      choices: ['android', 'ios'],
+      default: 'android'
     }];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
+      this.props = props;
+      
       done();
     }.bind(this));
   },
+  
+  //==========================================================================
+  // Create plugin
+  //--------------------------------------------------------------------------
+  // Run plugman: create plugin
+  //
+  plugman_create: function() {
+    assert(this.props.pluginName,    'pluginName is required');
+    assert(this.props.pluginID,      'pluginID is required');
+    assert(this.props.pluginVersion, 'pluginVersion is required');
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
-    },
-
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
+    var done = this.async();
+    
+    this.log('Start creating plugin');
+    plugman.create( this.props.pluginName,
+                    this.props.pluginID,
+                    this.props.pluginVersion,
+                    '.', [],
+      function(err) {
+        if(err){ console.log(err); }
+        else   { done(); }
+      });
   },
 
+  //-------------------------------------------------------------------------
+  // Run plugman: add platform
+  //
+  plugman_add_platforms: function() {
+    assert(this.props.pluginName,  'pluginName is required');
+    
+    this.log('Adding platform to plugin');
+    process.chdir(this.props.pluginName);
+    
+    for(var idx in this.props.pluginPlatforms) {
+      var platform = this.props.pluginPlatforms[idx];
+      this.log(' platform: ' + platform);
+
+      plugman.platform({ operation: 'add', platform_name: platform });
+    }
+
+    process.chdir('..');
+  },
+ 
+  
   install: function () {
+/*    
     this.installDependencies({
       skipInstall: this.options['skip-install']
     });
+*/
   }
 });
